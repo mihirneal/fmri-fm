0e92d886a55e9a8a72da8b3195082130382371e3
Mihir Tripathy <mihir.neal@icloud.com>
2025-12-26 21:19:50 +0000

    brainlm eval launch script

--- experiments/eval_v1/launch_eval.sh
+++ experiments/eval_v1/launch_eval.sh
@@ -23,21 +23,24 @@ EXP_DIR="experiments/${EXP_NAME}"
 OUT_DIR="${EXP_DIR}/output"
 
 # fill with the name of your home folder on lightning
-SHARE_USER=${SHARE_USER:-volunteer}
-SHARE_DIR="/teamspace/gcs_folders/share/fmri-fm/${SHARE_USER}"
+# SHARE_USER=${SHARE_USER:-volunteer}
+# SHARE_DIR="/teamspace/gcs_folders/share/fmri-fm/${SHARE_USER}"
 
-# save output to persistent shared storage
-SHARE_OUT_DIR="${SHARE_DIR}/${OUT_DIR}"
-mkdir -p ${SHARE_OUT_DIR} 2>/dev/null
-ln -sn ${SHARE_OUT_DIR} ${OUT_DIR} 2>/dev/null
+# # save output to persistent shared storage
+# SHARE_OUT_DIR="${SHARE_DIR}/${OUT_DIR}"
+# mkdir -p ${SHARE_OUT_DIR} 2>/dev/null
+# ln -sn ${SHARE_OUT_DIR} ${OUT_DIR} 2>/dev/null
 
 # download data locally
-export HCPYA_ROOT="/tmp/datasets/hcpya"
+export HCPYA_ROOT="/teamspace/studios/this_studio/HCPYA_processed"
+export BRAINLM_COORDS_DATASET_PATH="/teamspace/gcs_folders/share/fmri-fm/BrainLM/Brain_Region_Coordinates"
+export BRAINLM_REPO_PATH="/teamspace/studios/this_studio/BrainLM"
 mkdir -p $HCPYA_ROOT
 aws s3 sync s3://medarc/fmri-fm-eval/processed $HCPYA_ROOT \
     --exclude '*' \
     --include 'hcpya-rest1lr.flat.arrow*' \
     --include 'hcpya-rest1lr.schaefer400.arrow*' \
+    --include 'hcpya-rest1lr.a424.arrow*' \
     --include 'targets*'
 
 models=(
@@ -48,6 +51,12 @@ models=(
     flat_mae_base_patch16_2
     flat_mae_base_patch16_2
     flat_mae_base_patch16_2
+    brainlm_13m
+    brainlm_13m
+    brainlm_vitmae_111m
+    brainlm_vitmae_111m
+    brainlm_vitmae_650m
+    brainlm_vitmae_650m
 )
 reprs=(
     cls
@@ -57,6 +66,12 @@ reprs=(
     cls
     avg_patch
     patch
+    cls
+    avg_patch
+    cls
+    avg_patch
+    cls
+    avg_patch
 )
 model=${models[MODELID]}
 repr=${reprs[MODELID]}
@@ -69,9 +84,20 @@ datasets=(
     hcpya_rest1lr_pmat24
 )
 dataset=${datasets[TASKID]}
-
-overrides="representation=${repr} epochs=1 batch_size=4 lr=0.001 num_workers=16"
-overrides="${overrides} debug=true wandb=false"
+name="test_bs8"
+overrides="representation=${repr} name=${name} epochs=10 batch_size=8 lr=0.001 num_workers=16"
+overrides="${overrides} debug=false wandb=false device=cuda:0"
+if [[ $model == brainlm_* ]]; then
+    if [[ -z ${BRAINLM_COORDS_DATASET_PATH:-} ]]; then
+        echo "BRAINLM_COORDS_DATASET_PATH is required for BrainLM models."
+        exit 1
+    fi
+    if [[ -z ${BRAINLM_REPO_PATH:-} ]]; then
+        echo "BRAINLM_REPO_PATH is required for BrainLM models."
+        exit 1
+    fi
+    overrides="${overrides} model_kwargs.coords_dataset_path=${BRAINLM_COORDS_DATASET_PATH}"
+fi
 
 notes="initial probe eval run (${model}/${repr}/${dataset})."
 
